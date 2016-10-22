df4662c15ae4ea1a681777beab43bb1db1d24f03
Uli <ukoenig@Piratenfraktions-Mac-mini.local>
2016-10-22 23:23:36 +0200

    Erster Entwurf

--- PeWoSa/ViewController.swift
+++ PeWoSa/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+  
+    
+    private var webView: WKWebView?
+    
+    override func loadView() {
+        webView = WKWebView()
+        
+        //If you want to implement the delegate
+        //webView?.navigationDelegate = self
+        
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = UserDefaults.standard
+       defaults.set("User-Agent", forKey: "User-Agent")
+        
+        let url = URL(string: "https://ulikoenig.de/pewosa/info.php")
+        var requestObj = URLRequest(url: url!);
+        let userAgent = "de.ulikoenig.pewosa/android";
+        requestObj.setValue(userAgent, forHTTPHeaderField: "User-Agent")
+        webView?.load(requestObj);
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reloadContent(_ sender: UIBarButtonItem) {
+        webView?.reload()
+    }
 
+    @IBAction func loadStartpage(_ sender: AnyObject) {
+        let url = URL(string: "https://ulikoenig.de/pewosa/")
+        var requestObj = URLRequest(url: url!);
+        webView?.load(requestObj);
+    }
+    
+
+    
 }
 
